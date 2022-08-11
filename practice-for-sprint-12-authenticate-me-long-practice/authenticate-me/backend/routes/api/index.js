b47e357a00baba7b4ad2ca1da6a3d2e738f03d6b
George Merida <gmerida92@gmail.com>
2022-08-11 12:58:19 -0700

    Add User authentication Express middleware

--- practice-for-sprint-12-authenticate-me-long-practice/authenticate-me/backend/routes/api/index.js
+++ practice-for-sprint-12-authenticate-me-long-practice/authenticate-me/backend/routes/api/index.js
@@ -1,5 +1,7 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const { setTokenCookie } = require('../../utils/auth.js');
+
 
 // backend/routes/api/index.js
 // ...
@@ -7,6 +9,62 @@ const router = require('express').Router();
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
+
+// ...
+
+
+// backend/routes/api/index.js
+// ...
+
+// GET /api/set-token-cookie
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+});
+
+// ...
+
+
+// backend/routes/api/index.js
+// ...
+
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+router.get(
+    '/restore-user',
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+// ...
+
+
+// backend/routes/api/index.js
+// ...
+
+router.use(restoreUser);
+
+// ...
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
 
 // ...
 
