27772f0e0c18c55702bc7199d0b7bdb78be1d04b
George Merida <gmerida92@gmail.com>
2022-08-10 22:14:18 -0700

    Add User model authentication Sequelize scopes and methods

--- practice-for-sprint-12-authenticate-me-long-practice/authenticate-me/backend/db/models/user.js
+++ practice-for-sprint-12-authenticate-me-long-practice/authenticate-me/backend/db/models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model, Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -8,6 +9,48 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
+    toSafeObject() {
+      const { id, username, email } = this; //context will be the User instance
+      return { id, username, email };
+    }
+
+    validatePassword(password) {
+      return bcrypt.compareSynce(password, this.hashedPassword.toString());
+    }
+
+    static getCurrentyUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+
+
+    // Login method
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+
+    // Sign up method
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+
     static associate(models) {
       // define association here
     }
@@ -43,6 +86,19 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ["hashedPassword"] }
+      },
+      loginUser: {
+        attributes: {}
+      }
+    }
   });
   return User;
 };
