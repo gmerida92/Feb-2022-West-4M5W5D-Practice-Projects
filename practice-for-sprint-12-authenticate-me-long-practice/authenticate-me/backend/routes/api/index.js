12c858a165cc2b4752b1e8967936fa3ce7559d2a
George Merida <gmerida92@gmail.com>
2022-08-11 21:58:40 -0700

    last edits

--- practice-for-sprint-12-authenticate-me-long-practice/authenticate-me/backend/routes/api/index.js
+++ practice-for-sprint-12-authenticate-me-long-practice/authenticate-me/backend/routes/api/index.js
@@ -1,7 +1,11 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 const { setTokenCookie } = require('../../utils/auth.js');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
+router.use(restoreUser);
 
 // backend/routes/api/index.js
 // ...
@@ -13,59 +17,56 @@ router.post('/test', function (req, res) {
 // ...
 
 
-// backend/routes/api/index.js
-// ...
+// // backend/routes/api/index.js
+// // ...
 
-// GET /api/set-token-cookie
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-});
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// });
 
-// ...
-
-
-// backend/routes/api/index.js
-// ...
-
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
-
-router.use(restoreUser);
-
-router.get(
-    '/restore-user',
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
-
-// ...
+// // ...
 
 
-// backend/routes/api/index.js
-// ...
+// // backend/routes/api/index.js
+// // ...
 
-router.use(restoreUser);
+// // GET /api/restore-user
 
-// ...
+// router.use(restoreUser);
 
-// GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
+// router.get(
+//     '/restore-user',
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 
-// ...
+// // ...
+
+
+// // backend/routes/api/index.js
+// // ...
+
+// router.use(restoreUser);
+
+// // ...
+
+// // GET /api/require-auth
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// // ...
 
 module.exports = router;
