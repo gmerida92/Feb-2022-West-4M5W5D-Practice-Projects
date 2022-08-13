173560c4a5ac6ab8e3467a0bb68342b49c1ca0e2
George Merida <gmerida92@gmail.com>
2022-08-13 12:19:54 -0700

    Add backend endpoint to get the current user session

--- practice-for-sprint-12-authenticate-me-long-practice/authenticate-me/backend/routes/api/session.js
+++ practice-for-sprint-12-authenticate-me-long-practice/authenticate-me/backend/routes/api/session.js
@@ -34,4 +34,18 @@ router.delete('/', async (req, res, next) => {
     });
 });
 
+// Restore session user
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    
+    if (user) {
+        return res.json({
+            user: user.toSafeObject()
+        });
+    }
+    else {
+        return res.json({});
+    };
+});
+
 module.exports = router;
